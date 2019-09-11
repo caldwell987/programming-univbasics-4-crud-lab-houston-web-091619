096fc6a152a4526a65b376e9ddfc1a3893c31428
David Caldwell <david@avvitt.com>
2019-09-11 20:13:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,9 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  family = ["David","John","Wes","Michael"]
+  family.unshift ("arrays!")
+  p family
 end
 
 def remove_element_from_end_of_array(array)
