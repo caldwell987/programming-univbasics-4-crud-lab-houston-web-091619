017fb9069533df3ccaf55b3cebc798d9edef1889
David Caldwell <david@avvitt.com>
2019-09-11 20:24:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,7 +21,7 @@ end
 def remove_element_from_end_of_array(array)
   family = ["David","John","Wes","Michael","arrays!"]
   family.pop
-  p arrays!
+  p "arrays!"
 end
 
 def remove_element_from_start_of_array(array)
