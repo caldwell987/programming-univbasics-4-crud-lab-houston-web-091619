53927e714d05224efebdb9b7f526b185d4c6d385
David Caldwell <david@avvitt.com>
2019-09-11 20:10:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  family = ["David","John","Wes","Michael"]
-  family.push ("Sandy")
+  people.push ("Sandy")
 end
 
 def add_element_to_start_of_array(array, element)
